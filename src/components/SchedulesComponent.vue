9cbd3dd77c6725b621d13422ba6096fe3b854d66
Zero <samuelbustos10903@gmail.com>
2024-12-10 16:10:18 -0300

    feat: added schedules section

--- src/components/SchedulesComponent.vue
+++ src/components/SchedulesComponent.vue
@@ -1,7 +1,29 @@
 <template>
-	<section>Schedules</section>
+	<section class="schedules">
+		<h2 class="schedules__title">Horarios</h2>
+		<img
+			src="../assets/images/schedules.svg"
+			alt="schedules image"
+			class="schedules__image" />
+		<p>
+			El local abre de <span class="accent-color">martes a domingo</span> a
+			partir de las <span class="accent-color">18hs</span>. En algunas
+			ocasiones, es probable que abra a la mañana. En cualquier caso, consultar
+			vía whatsapp.
+		</p>
+	</section>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/sass/layout';
+@use '../assets/sass/config' as *;
+
+.schedules {
+	@include layout.flexCenter(column, 1rem);
+	&__image {
+		filter: drop-shadow($dark-shadow);
+	}
+}
+</style>
